fe2a186f675e41a7634f34d1ec0fe57403402db9
Arnel-rah <arnel.raharinandrasana@student.passerellesnumeriques.org>
2026-03-15 22:43:08 +0300

    feat(service): create pokemonService to fetch Pokemons from API

---
+++ src/services/pokemonService.ts
@@ -0,0 +1,34 @@
+import { Pokemon } from "../types/pokemon"
+
+export const fetchPokemons = async (): Promise<Pokemon[]> => {
+
+  const response = await fetch(
+    "https://pokeapi.co/api/v2/pokemon?limit=15"
+  )
+
+  if (!response.ok) {
+    throw new Error("HTTP Error")
+  }
+
+  const list = await response.json()
+
+  const details = await Promise.all(
+    list.results.map(async (item: { name: string; url: string }) => {
+
+      const res = await fetch(item.url)
+      const pokemon = await res.json()
+
+      return {
+        id: pokemon.id,
+        name: pokemon.name,
+        image: pokemon.sprites.other["official-artwork"].front_default,
+        types: pokemon.types.map((t: any) => t.type.name),
+        height: pokemon.height,
+        weight: pokemon.weight
+      } as Pokemon
+
+    })
+  )
+
+  return details
+}
