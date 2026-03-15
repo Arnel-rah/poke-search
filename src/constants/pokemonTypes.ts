f0261d97ee902a4cf485bde43413ab0953784ee4
Arnel-rah <arnel.raharinandrasana@student.passerellesnumeriques.org>
2026-03-15 22:40:56 +0300

    feat(constants): add POKEMON_TYPES and PokemonType union

---
+++ src/constants/pokemonTypes.ts
@@ -0,0 +1,8 @@
+export const POKEMON_TYPES = [
+  "fire","water","grass","electric","psychic",
+  "ice","dragon","dark","fairy","normal",
+  "fighting","flying","poison","ground","rock",
+  "bug","ghost","steel"
+] as const
+
+export type PokemonType = typeof POKEMON_TYPES[number]
